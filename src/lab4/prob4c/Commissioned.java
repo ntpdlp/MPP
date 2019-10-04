4b10c071a50602d7ee11ad3e4ad24943a91019fd
duc <duc@192.168.0.234>
2019-10-04 13:10:42 -0500

    update for countPreviousMonthOrders()

--- src/lab4/prob4c/Commissioned.java
+++ src/lab4/prob4c/Commissioned.java
@@ -25,17 +25,9 @@ public class Commissioned extends Employee{
 	
 	//functions
 	public int countPreviousMonthOrders(int month, int year) {
-		//get list date of orders, count if its month is previous month
-		int countMonth;
-		int countYear;
-		if(month >=2) {
-			countMonth = month - 1;
-			countYear = year;
-		}else {
-			//if currentMonth is January, its value = 1 => previousMonth = 12 of (year - 1)
-			countMonth = 12;
-			countYear = year - 1; 
-		}
+		LocalDate ldate = LocalDate.of(year, month, 1).minusMonths(1);
+		int countMonth = ldate.getMonthValue();
+		int countYear = ldate.getYear();
 		
 		//count the previous orders
 		int count = 0;
