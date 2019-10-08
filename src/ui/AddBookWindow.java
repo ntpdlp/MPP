83704aea7b6e1c0833a0009968269684e59f5b2a
duc <duc@10.10.61.73>
2019-10-08 12:06:16 -0500

    addBook

--- src/ui/AddBookWindow.java
+++ src/ui/AddBookWindow.java
@@ -1,19 +1,33 @@
 package ui;
 
+import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 import javafx.stage.Stage;
 
 public class AddBookWindow extends Stage implements LibWindow{
 	//fields
+	@FXML
 	private TextField txtTitle;
+	@FXML
 	private TextField txtIsbn;
+	@FXML
 	private ComboBox<String> cboAuthors;
+	@FXML
+	private TextArea txaAuthorListDisplay;
+	@FXML
+	private Button btnAddNewAuthor;
+	@FXML
 	private TextField txtAvailability;
+	@FXML
 	private TextField txtCopies;
+	@FXML
 	private Button btnBackMain;
+	@FXML
 	private Button btnAdd;
+	@FXML
 	private Button btnCancel;
 	
 
