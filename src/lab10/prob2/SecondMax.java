fa8ccccaee80bb863b86aa3a684ed884cada7b52
duc <duc@mbp-cua-duc.cs.mum.edu>
2019-10-23 07:10:07 -0500

    lab10,prob2

--- src/lab10/prob2/SecondMax.java
+++ src/lab10/prob2/SecondMax.java
@@ -8,44 +8,44 @@ public class SecondMax {
 
 	public static void main(String[] args) {
 		List<Integer> ints = new ArrayList<>();
-		ints.add(3);ints.add(4);ints.add(8);ints.add(20); //3 4 8 20
+		ints.add(3);
+		ints.add(4);
+		ints.add(8);
+		ints.add(20); // 3 4 8 20
 
-		
 		List<String> strs = new ArrayList<>();
-		strs.add("Bb"); strs.add("Ss"); strs.add("Aa"); strs.add("Zz"); // Aa Bb Ss Zz 
+		strs.add("Bob");
+		strs.add("Susan");
+		strs.add("Alice");
+		strs.add("Zera"); // Alice Bob Susan Zera
 		
-		System.out.println(maxSecond(ints,new IntegerComparator()));
-		System.out.println(maxSecond(strs,new StringComparator()));
+		
+		List<Float> floats = new ArrayList<>();
+		floats.add(2.3f);
+		floats.add(4.5f);
+		floats.add(10.0f);
+		floats.add(5.0f); //2.3 4.5 5.0 10.0
 
-	}//end_main
-	
-	
-	public static <T> T maxSecond(List<? extends T> list, Comparator<? super T> cmp) {
+		System.out.println(maxSecond(ints, new AnyComparator<Integer>()));
+		System.out.println(maxSecond(strs, new AnyComparator<String>()));
+		System.out.println(maxSecond(floats, new AnyComparator<Float>()));
+		
+	}// end_main
+
+	public static <T> T maxSecond(List<? extends T> list, Comparator<T> cmp) {
 		list.sort(cmp);
-		return list.get(list.size()-2);
+		return list.get(list.size() - 2);
 	}
 
 	
-	static class IntegerComparator implements Comparator<Integer> {
+	static class AnyComparator<T extends Comparable<T>> implements Comparator<T> {
 
 		@Override
-		public int compare(Integer o1, Integer o2) {
+		public int compare(T o1, T o2) {
+			// TODO Auto-generated method stub
 			return o1.compareTo(o2);
-			
 		}
-		
-	}//endcomparator
-	
-	static class StringComparator implements Comparator<String> {
+	}// endcomparator
 
-		@Override
-		public int compare(String o1, String o2) {
-			return o1.compareTo(o2);
-			
-		}
-		
-	}
 
-	
-	
 }//end_class
